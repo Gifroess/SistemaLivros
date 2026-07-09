510be0226746d955343de5ab97a918ead2f3cda1
Isabela Montor <isabelamontor03@gmail.com>
2026-07-09 10:10:49 -0300

    adição dos nomes dos integrantes no footer

--- frontend-livros-main/src/componentes/Footer/Footer.tsx
+++ frontend-livros-main/src/componentes/Footer/Footer.tsx
@@ -5,7 +5,20 @@ export default function Footer(){
     return(
         <footer className='footer'>
             <section>
-                <p style={{textAlign: "center"}}>Feito com 🎮 por <a href="https://www.github.com/phillcisco" target='_blank'>philcisco</a></p>
+                <p style={{textAlign: "center"}}>
+                    Feito com 🎮 por{" "}
+                    <a href="https://github.com/Gifroess" target="_blank">
+                        Giovana Fróes
+                    </a>
+                    ,{" "}
+                    <a href="https://github.com/montorisabela" target="_blank">
+                        Isabela Montor
+                    </a>{" "}
+                    e{" "}
+                    <a href="https://github.com/nottfael" target="_blank">
+                        Rafael Leite
+                    </a>
+                </p>
             </section>
         </footer>
     );
